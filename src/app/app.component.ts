69f01e78ec4b6946d95bb0811ab31ced31ff6166
jayacerlaptop <jayacerlaptop@gmail.com>
2018-05-06 11:57:31 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
-    
-    // `<div>
-    //                 <h1>{{pageHeader}}</h1>
-    //                 <my-employee></my-employee>
-    //            </div>`
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+
+  // `<div>
+  //                 <h1>{{pageHeader}}</h1>
+  //                 <my-employee></my-employee>
+  //            </div>`
 })
 export class AppComponent {
-    pageHeader: string = 'Employee Details';
+  pageHeader: string = 'Employee Details';
+  firstName: string = 'Tom';
+  lastName: string = 'Hopkins';
+  isDisabled: boolean = false;
+
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
 }
