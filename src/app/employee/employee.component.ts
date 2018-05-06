b89a6627911593bba832e785eff2547030680021
jayacerlaptop <jayacerlaptop@gmail.com>
2018-05-06 11:22:59 -0400

    1

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,7 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-employee',
-    templateUrl: 'app/employee/employee.component.html'
+    templateUrl: './employee.component.html',
+    styleUrls: ['./employee.component.css']
+//     `<table>
+//     <tr>
+//     <td>First Name</td>
+//     <td>{{firstName}}</td>
+//     </tr>
+//     <tr>
+//     <td>Last Name</td>
+//         <td>{{lastName}}</td>
+//     </tr>
+//     <tr>
+//         <td>Gender</td>
+//         <td>{{gender}}</td>
+//     </tr>
+//     <tr>
+//         <td>Age</td>
+//         <td>{{age}}</td>
+//     </tr>
+// </table>`
 })
 export class EmployeeComponent {
     firstName: string = 'Tom';
