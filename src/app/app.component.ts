cbfeffb298953c7a4ebd27af3c52d91437b92f39
jayacerlaptop <jayacerlaptop@gmail.com>
2018-05-06 10:42:27 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `<div>
+                    <h1>{{pageHeader}}</h1>
+               </div>`
 })
 export class AppComponent {
-  title = 'app';
+    pageHeader: string = 'Employee Details';
 }
